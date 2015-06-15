8d0acf87230dfe410a0a328ce255485cda538b27
RossLitz <codingclasses.049@gmail.com>
2015-06-15 16:03:29 -0500

    created the save badges

--- app.js
+++ app.js
@@ -2,5 +2,11 @@
 
 var express = require('express');
 var app = express();
+var badges = require('./controllers/badges');
 
 app.use(express.json());
+
+app.post('/', badges.save, badges.send);
+
+
+app.listen(8000);
